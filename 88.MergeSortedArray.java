0aab8885b0cda5fd65f7dc1616fce5cf8a26b9b3
Kim_TaeHyeong <37135317+KimTaeHyeong17@users.noreply.github.com>
2020-04-10 02:08:43 +0900

    Create 88.MergeSortedArray.java

---
+++ 88.MergeSortedArray.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        for(int i = m ; i < nums1.length ; i++){
+            nums1[i] = nums2[i-m]; 
+        }
+        Arrays.sort(nums1);
+    }
+}
