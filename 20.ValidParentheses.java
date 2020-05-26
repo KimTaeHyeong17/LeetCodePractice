3697a5be4b37df2d4c21e80725da449cf7e36050
Kim_TaeHyeong <37135317+KimTaeHyeong17@users.noreply.github.com>
2020-05-26 16:05:46 +0900

    Update 20.ValidParentheses.java

--- 20.ValidParentheses.java
+++ 20.ValidParentheses.java
@@ -29,3 +29,42 @@ class Solution {
         }
     }
 }
+
+==============================================================
+    
+    import java.util.*;
+class Solution {
+    public boolean isValid(String s) {
+        HashMap<Character,Character> map = new HashMap();
+        map.put('(',')');
+        map.put('[',']');
+        map.put('{','}');
+        map.put(')','@');
+        map.put(']','@');
+        map.put('}','@');
+
+        Stack<Character> stack = new Stack<>();
+        
+        for(int  i = 0 ; i < s.length() ; i++){
+            char c = s.charAt(i);
+          
+            if(stack.isEmpty() == true){
+                stack.push(c);
+            }else{
+                if((map.get(stack.peek())) == c){
+                    stack.pop();
+                }else{
+                    stack.push(c);
+                }
+            }
+        }
+        // System.out.println(stack);
+        
+        if(stack.isEmpty() == true){
+            return true;
+        }else{
+            return false;
+        }
+        
+    }
+}
